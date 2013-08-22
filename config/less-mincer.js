9ba4cdb5d02c3214a3120cb078b8bfb8a694c1dc
Denis Bardadym <bardadymchik@gmail.com>
2013-08-22 10:02:18 +0400

    Update for last mincer

--- config/less-mincer.js
+++ config/less-mincer.js
@@ -9,30 +9,6 @@ var installLessSupport = function(environment) {
             return "url('" + this[assetType + 'Path'](pathname) + "')";
         });
     });
-
-    var originalLessEvaluate = LessEngine.prototype.evaluate;
-
-    LessEngine.prototype.evaluate = function (context, locals) {
-
-        var localNames = Object.keys(locals);
-
-        // add locals to less functions
-        localNames.forEach(function(local) {
-            less.tree.functions[local] = function(lessArg) {
-                return new less.tree.Anonymous(locals[local](lessArg.value));
-            }
-        });
-
-        var result = originalLessEvaluate.call(this, context);
-
-        //remove locals from less functions
-
-        localNames.forEach(function(local) {
-            delete less.tree.functions[local];
-        });
-
-        return result;
-    }
 };
 
 module.exports = installLessSupport;
