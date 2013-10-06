fa7cd57d4a8a42e2c6a44894fd169eb4f4cab43b
Denis Bardadym <bardadymchik@gmail.com>
2013-10-06 13:02:53 +0400

    Make grunt working. To run just type `grunt`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,31 @@
+var path = require('path'),
+    fs = require('fs')
+
+var bowerPath = './bower_components';
+
+try {
+    bowerPath = JSON.parse(fs.readFileSync('./.bowerrc', 'utf8')).directory;
+} catch(e) {}
+
+var bower = function(subPath) {
+    return path.join(bowerPath, subPath);
+};
+
+var alias = function(path, aliasName) {
+    return path + ':' + aliasName;
+};
+
+var bowerAlias = function(path, aliasName) {
+    return alias(bower(path), aliasName);
+};
+
+var nodeAlias = function(subPath, aliasName) {
+    return alias(path.join('./node_modules', subPath), aliasName);
+};
+
 module.exports = function(grunt) {
+
+
 
     grunt.initConfig({
 
@@ -13,49 +40,43 @@ module.exports = function(grunt) {
 
             watch: {
                 publicJs: {
-                    files: ['public/js/**/*.js', 'public/js/components/**/*.jsx', 'public/templates/**/*.text', '!public/js/apps/*'],
-                    tasks: ['react', 'lmd', 'develop'],
-                    options: {
-                        //interrupt: true
-                        //spawn: false
-                    }
+                    files: ['public/js/**/*.js', 'public/js/components/**/*.jsx', '!public/js/apps/*'],
+                    tasks: ['browserify']
                 },
                 app: {
                     files: ['app/**/*.js', 'config/**/*.js', 'app/**/*.jade'],
-                    tasks: ['develop'],
-                    options: {
-                        //interrupt: true
-                        //spawn: false
-                    }
+                    tasks: ['develop']
                 }
             },
 
-            lmd: {
-                publicJs: {
-                    build: 'default'
-                }
-            },
-            
-            react: {
-                options: {
-                    extension: 'jsx'
-                },
-                app: {
-                    files: {
-                        'public/js/components/built': 'public/js/components/jsx'
+            browserify: {
+                mainJs: {
+                    src: ['public/js/main.js'],
+                    dest: 'public/js/apps/main.js',
+                    options: {
+                        alias: [
+                            bowerAlias('page.js/index.js', 'page'),
+                            nodeAlias('react-tools/build/modules/React.js', 'react'),
+                            bowerAlias('socket.io-client/dist/socket.io.js', 'socket.io'),
+                            bowerAlias('lodash/dist/lodash.js', 'lodash')
+                        ],
+                        extensions: [".jsx"],
+                        shim: {
+                            jquery: { path: "./public/bower_components/jquery/jquery.js", exports: "$" }
+                        },
+                        debug: true,
+                        transform: ['reactify']
                     }
                 }
-            },
+            }
 
         });
 
         // A very basic default task.
-        grunt.registerTask('react', ['react']);
-        grunt.registerTask('default', ['develop', 'react', 'lmd', 'watch']);
+        grunt.registerTask('default', ['develop', 'browserify', 'watch']);
 
-        grunt.loadNpmTasks('grunt-contrib-watch'); 
-        grunt.loadNpmTasks('grunt-react');
-        grunt.loadNpmTasks('grunt-lmd'); 
+        grunt.loadNpmTasks('grunt-contrib-watch');
+        grunt.loadNpmTasks('grunt-browserify');
         grunt.loadNpmTasks('grunt-develop'); 
 
 
