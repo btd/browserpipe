ad99b41a84c176ee9b836d763fc97ff7d9388bba
Denis Bardadym <bardadymchik@gmail.com>
2013-10-23 22:26:08 +0400

    Hide not tested thing

--- api/express.js
+++ api/express.js
@@ -1,8 +1,6 @@
 var express = require('express');
 require('express-namespace');
 
-var logger = require('intel').getLogger('connect');
-
 var app = express();
 
 app.set('views', __dirname + '/view');
@@ -16,11 +14,11 @@ app.configure('development', function () {
     app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
     //set up colour logger for dev
-    app.use(express.logger({ format: 'dev', stream: {
+    /*app.use(express.logger({ format: 'dev', stream: {
        write: function(msg) {
            logger.info(msg.substr(0, msg.length - 1))
        }
-    }}));
+    }}));*/
 });
 
 module.exports = app;
