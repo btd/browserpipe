2430529287c2220fb79e230e4a0f520539b50963
Daniel Fernandez <fernandez.daniel@gmail.com>
2013-11-15 13:26:51 -0300

    fixed reviewed requests

--- extensions/chrome/extension/sync.js
+++ extensions/chrome/extension/sync.js
@@ -143,9 +143,3 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 
     });
 });
-
-//var port = chrome.extension.connect();
-document.getElementById('myCustomEventDiv').addEventListener('myCustomEvent', function() {
-    var eventData = document.getElementById('myCustomEventDiv').innerText;
-    //port.postMessage({message: "myCustomEvent", values: eventData});
-});
